f52cb5dfe714f18c05ade505b8fefe2c9e0782f7
saymmoa <saymmoa@users.noreply.github.com>
2024-12-12 04:16:50 +0000

    deploy: 3bb596db73ef9c1f246b47ef12b5ba438ad676ad

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "Privacy Policy",
+        "excerpt":"세이모아 개인정보 처리방침 This is the Privacy Policy page. It explains how we collect, use, and protect your data.   ","categories": [],
+        "tags": [],
+        "url": "/2024/12/11/privacy-policy.html",
+        "teaser": null
+      },{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/jekyll/update/2024/12/11/welcome-to-jekyll.html",
+        "teaser": null
+      }]
